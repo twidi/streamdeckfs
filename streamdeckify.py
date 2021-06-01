4daf3b89320f9adc2ad8a21d6541ca611f96f735
Stephane Angel (Twidi) <s.angel@twidi.com>
2021-06-01 15:14:13 +0200

    Total rewrite of files watcher
    - Automatically handle checking of parents removal
    - base class with only the needed method to override to make a files
    watcher that does not depends on inotify

--- streamdeckify.py
+++ streamdeckify.py
@@ -181,103 +181,363 @@ VersionProxyMostRecent = partial(VersionProxy, sort_key_func=lambda key_and_obj:
 versions_dict_factory = lambda: defaultdict(VersionProxyMostRecent)
 
 
-class Inotifier:
-    name = 'FilesWatcher'
+class WatchedDirectory:
 
-    WATCHED_DIR_REMOVED = f.DELETE_SELF | f.MOVE_SELF | f.UNMOUNT
+    by_directories = {}
+    by_watch_ids = {}
+
+    @staticmethod
+    def normalize_directory(directory):
+        if not isinstance(directory, Path):
+            directory = Path(directory)
+        return directory
+
+    @classmethod
+    def add(cls, files_watcher, directory, watcher=None):
+        if not (watched := cls.get_by_directory(directory)):
+            watched = cls(files_watcher, directory)
+            cls.by_directories[watched.directory] = watched
+            if (parent_directory := watched.directory.parent) != directory:
+                watched.parent = watched.add(files_watcher, parent_directory)
+                watched.parent.children.append(watched)
+            else:
+                watched.parent = None
+        if watcher:
+            watched.add_watcher(watcher)
+        else:
+            watched.update_watch()
+        return watched
+
+    @classmethod
+    def remove(cls, directory, watcher):
+        if not (watched := cls.get_by_directory(directory)):
+            return
+        watched.remove_watcher(watcher)
+        watched.update_watch()
+
+    @classmethod
+    def get_by_directory(cls, directory):
+        return cls.by_directories.get(cls.normalize_directory(directory))
+
+    @classmethod
+    def get_by_watch_id(cls, watch_id):
+        return cls.by_watch_ids.get(watch_id)
+
+    def __init__(self, files_watcher, directory):
+        self.files_watcher = files_watcher
+        self.directory = self.normalize_directory(directory)
+        self.name = self.directory.name
+        self.parent = None
+        self.children = []
+        self.watchers = []
+        self.watch_mode = None
+        self.watch_id = None
+        self.exists = self.directory_exists()
+
+    def __str__(self):
+        return f'Watched directory: {self.directory} ; Exists={self.directory_exists()} ({self.exists}) ; Mode={self.get_mode()} ({self.watch_mode} ; WatchId={self.watch_id})'
+
+    def __repr__(self):
+        return f'<WatchedDirectory "{self.directory}">'
+
+    def directory_exists(self):
+        return self.directory.exists() and self.directory.is_dir()
+
+    @property
+    def waiting(self):
+        return not self.exists
+
+    def add_watcher(self, watcher):
+        if watcher not in self.watchers:
+            self.watchers.append(watcher)
+            self.update_watch()
+
+    def remove_watcher(self, watcher):
+        try:
+            self.watchers.remove(watcher)
+        except ValueError:
+            pass
+        else:
+            self.update_watch()
+
+    def has_waiting_children(self):
+        return any(child.waiting for child in self.children)
+
+    def has_descendant_watchers(self):
+        return any(bool(child.watchers) or child.has_descendant_watchers() for child in self.children)
+
+    def get_mode(self):
+        if not self.watchers and not self.has_descendant_watchers():
+            # if their is noone waiting for us we can stop watching the directory
+            return None
+        if self.waiting:
+            # if the directory does not exist, we are in waiting mode, ie not watched but the parent will be
+            # in "all" watch mode to know when the directory is created
+            return 'waiting'
+        if self.watchers:
+            # if we have direct watchers, we watch for content and self-deletion
+            return 'all'
+        if self.has_waiting_children():
+            # if we have no direct watchers but have direct children in waiting mode (ie their directory does not exist)
+            # we watch for content and self-deletion
+            return 'all'
+        # if we have no direct watchers and no direct children in waiting mode, we only watch for self-deletion
+        return 'self_delete'
+
+    def update_watch(self):
+        self.exists = self.directory_exists()
+        if (watch_mode := self.get_mode()) == self.watch_mode:
+            return
+        self.watch_mode = watch_mode
+        if self.watch_mode in ('waiting', None):
+            if self.files_watcher and self.watch_id:
+                self.files_watcher.remove_watch(self.watch_id)
+        else:
+            if self.files_watcher:
+                self.watch_id = self.files_watcher.set_watch(self.directory, self.watch_mode)
+
+        if self.parent:
+            self.parent.update_watch()
+
+    @classmethod
+    def on_watch_set(cls, directory, watch_id):
+        if not (watched := cls.get_by_directory(directory)):
+            return
+        watched.watch_id = watch_id
+        cls.by_watch_ids[watch_id] = watched
+
+    @classmethod
+    def on_watch_removed(cls, watch_id):
+        if not (watched := cls.get_by_watch_id(watch_id)):
+            return
+        cls.by_watch_ids.pop(watched.watch_id, None)
+        watched.watch_id = None
+
+    def on_file_added(self, name):
+        if not self.watchers:
+            return
+        for watcher in self.watchers:
+            watcher.on_file_change(self.directory, name, f.CREATE, time())
+
+    def on_file_removed(self, name):
+        if not self.watchers:
+            return
+        for watcher in self.watchers:
+            watcher.on_file_change(self.directory, name, f.DELETE, time())
+
+    def on_file_changed(self, name):
+        if not self.watchers:
+            return
+        for watcher in self.watchers:
+            watcher.on_file_change(self.directory, name, f.MODIFY, time())
+
+    def get_child(self, name):
+        try:
+            return next(child for child in self.children if child.name == name)
+        except StopIteration:
+            return None
+
+    def on_directory_added(self, name):
+        if (child := self.get_child(name)):
+            child.update_watch()
+            for grand_child in child.iter_all_children():
+                grand_child.update_watch()
+        for watcher in self.watchers:
+            watcher.on_file_change(self.directory, name, f.CREATE | f.ISDIR, time())
+
+    def on_directory_removed(self, name):
+        if (child := self.get_child(name)):
+            child.update_watch()
+            for grand_child in child.iter_all_children():
+                grand_child.update_watch()
+        for watcher in self.watchers:
+            watcher.on_file_change(self.directory, name, f.DELETE | f.ISDIR, time())
+
+    def iter_all_children(self):
+        """Yields all children from the current object and its children, starting by the most far in the tree"""
+        for child in self.children:
+            yield from child.iter_all_children()
+            yield child
+
+    def iter_all_watchers(self):
+        """Yields all (object, watchers) from the current object and its children, starting by the most far in the tree"""
+        for child in self.iter_all_children():
+            for watcher in child.watchers:
+                yield child, watcher
+        for watcher in self.watchers:
+            yield self, watcher
+
+    def on_self_directory_removed(self, directory=None):
+        if not directory:
+            directory = self.directory
+        for children in self.iter_all_children():
+            children.on_self_directory_removed(directory)
+        self.update_watch()
+        for watched, watcher in self.iter_all_watchers():
+            watcher.on_directory_removed(directory)
+
+
+class BaseFilesWatcher:
+    WatchedDirectory = WatchedDirectory
+    thread_name = 'FilesWatcher'
 
     def __init__(self):
         self.running = False
-        self.inotify = INotify()
-        self.watchers = {}
-        self.watched = {}
+
+    def set_watch(self, directory, watch_mode):
+        try:
+            watch_id = self._set_watch(directory, watch_mode)
+        except Exception as exc:
+            logger.exception(f'[{self.thread_name}] Could not watch directory "{directory}" in mode "{watch_mode}": {exc}')
+        else:
+            self.WatchedDirectory.on_watch_set(directory, watch_id)
+
+    def _set_watch(self, directory, watch_mode):
+        # must return a watch id
+        raise NotImplementedError
+
+    def remove_watch(self, watch_id):
+        try:
+            self._remove_watch(watch_id)
+        except Exception as exc:
+            logger.exception(f'[{self.thread_name}] Could not remove watch "{watch_id}": {exc}')
+        else:
+            self.WatchedDirectory.on_watch_removed(watch_id)
+
+    def _remove_watch(self, watch_id):
+        raise NotImplementedError
 
     def stop(self):
         self.running = False
+
+    def stopped(self):
+        return not self.running
+
+    def run(self):
+        set_thread_name(self.thread_name)
+        self.running = True
+        while True:
+            if self.stopped():
+                break
+            for event in self.iter_events():
+                if self.stopped():
+                    break
+                watch_id = self.get_event_watch_id(event)
+                if not (watched := self.WatchedDirectory.get_by_watch_id(watch_id)):
+                    continue
+                name = self.get_event_watch_name(event)
+                if self.is_event_self_removed(event):
+                    watched.on_self_directory_removed()
+                elif self.is_event_directory_added(event):
+                    watched.on_directory_added(name)
+                elif self.is_event_directory_removed(event):
+                    watched.on_directory_removed(name)
+                elif self.is_file_added(event):
+                    watched.on_file_added(name)
+                elif self.is_file_removed(event):
+                    watched.on_file_removed(name)
+                elif self.is_file_changed(event):
+                    watched.on_file_changed(name)
+
+    def iter_events(self):
+        raise NotImplementedError
+
+    def get_event_watch_id(self, event):
+        raise NotImplementedError
+
+    def get_event_watch_name(self, event):
+        raise NotImplementedError
+
+    def is_event_self_removed(sellf, event):
+        raise NotImplementedError
+
+    def is_event_directory_added(self, event):
+        raise NotImplementedError
+
+    def is_event_directory_removed(self, event):
+        raise NotImplementedError
+
+    def is_file_added(self, event):
+        raise NotImplementedError
+
+    def is_file_removed(self, event):
+        raise NotImplementedError
+
+    def is_file_changed(self, event):
+        raise NotImplementedError
+
+
+class InotifyFilesWatcher(BaseFilesWatcher):
+
+    def __init__(self):
+        super().__init__()
+        self.inotify = INotify()
+        self.mapping = {}  # only used to display directories in debug mode
+
+    flag_groups = {
+        'self_delete': f.DELETE_SELF | f.MOVE_SELF | f.UNMOUNT,
+        'all': f.CREATE | f.DELETE | f.MODIFY | f.MOVED_FROM | f.MOVED_TO | f.DELETE_SELF | f.MOVE_SELF | f.UNMOUNT,
+        'added': f.ISDIR | f.CREATE | f.MOVED_TO,
+        'removed': f.ISDIR | f.DELETE | f.MOVED_FROM,
+        'changed': f.MODIFY,
+    }
+
+    def _set_watch(self, directory, watch_mode):
+        watch_id = self.inotify.add_watch(directory, self.flag_groups[watch_mode])
+        self.mapping[watch_id] = directory
+        return watch_id
+
+    def _remove_watch(self, watch_id):
+        self.inotify.rm_watch(watch_id)
+        self.mapping.pop(watch_id, None)
+
+    def stop(self):
+        super().stop()
         if self.inotify:
             self.inotify.close()
 
-    def add_watch(self, directory, owner, flags=f.CREATE | f.DELETE | f.MODIFY | f.MOVED_FROM | f.MOVED_TO | f.DELETE_SELF | f.MOVE_SELF | f.UNMOUNT):
-        if not isinstance(directory, Path):
-            directory = Path(directory)
-        if directory not in self.watched:
-            self.watched[directory] = {
-                'watcher': (watcher := self.inotify.add_watch(directory, flags)),
-                'owners': [],
-            }
-            self.watchers[watcher] = directory
-        elif owner in self.watched[directory]['owners']:
-            return
-        self.watched[directory]['owners'].append(owner)
-        # add a watcher on all parent directories to be notified when the directory is removed
-        for parent in directory.parents:
-            # we don't tie the watcher to a specific owner: if the directory is removed, we'll go through all
-            # watched dirs and if they are sub dirs, we'll go through all owners
-            self.add_watch(parent, None)
+    def stopped(self):
+        return super().stopped() or self.inotify.closed
 
+    def iter_events(self):
+        try:
+            for event in self.inotify.read(timeout=500):
+                directory = self.mapping.get(event.wd)
+                logger.debug(f'{event} ; {directory}/{event.name} ; FLAGS: {", ".join(str(flag) for flag in f.from_mask(event.mask))}')
+                if event.mask & f.IGNORED:
+                    self.remove_watch(event.wd)
+                    continue
+                yield event
+        except ValueError:
+            # happen if read while closed
+            pass
 
-    def remove_watch(self, directory, owner=None):
-        if not isinstance(directory, Path):
-            directory = Path(directory)
-        if directory not in self.watched:
-            return
-        if owner:
-            try:
-                self.watched[directory]['owners'].remove(owner)
-            except ValueError:
-                pass
-        owners_left = self.watched[directory]['owners']
-        watcher = self.watched[directory]['watcher']
-        if not owner or not owners_left:
-            del self.watched[directory]
-            try:
-                self.inotify.rm_watch(watcher)
-            except OSError:
-                # can happen when a directory is deleted, the watcher is removed on the kernel side
-                pass
-            self.watchers.pop(watcher)
+    def get_event_watch_id(self, event):
+        return event.wd
 
-    def run(self):
-        set_thread_name(self.name)
-        self.running = True
-        while True:
-            if not self.running or self.inotify.closed:
-                break
-            try:
-                for event in self.inotify.read(timeout=500):
-                    if not self.running or self.inotify.closed:
-                        break
-                    watcher, name, flags = event.wd, event.name, event.mask
-                    if watcher not in self.watchers:
-                        continue
-                    directory = self.watchers[watcher]
-                    logger.debug(f'{event} ; {directory}/{event.name} ; FLAGS: {", ".join(str(flag) for flag in f.from_mask(event.mask))}')
-                    if directory in self.watched:
-                        owners = self.watched[directory]['owners']
-                    else:
-                        owners = None
-                    if flags & f.IGNORED:
-                        if watcher in self.watchers:
-                            self.remove_watch(directory)
-                        continue
-                    if flags & self.WATCHED_DIR_REMOVED:
-                        notified = set()
-                        for watched_directory in sorted(self.watched.keys(), reverse=True):
-                            if watched_directory.is_relative_to(directory):
-                                for owner in self.watched[watched_directory]['owners']:
-                                    if not owner or owner in notified:
-                                        continue
-                                    notified.add(owner)
-                                    owner.on_directory_removed(directory, flags)
-                                self.remove_watch(watched_directory)
-                        self.remove_watch(directory)
-                    else:
-                        for owner in owners:
-                            if owner:
-                                owner.on_file_change(directory, name, flags, time())
-            except ValueError:
-                # happen if read while closed
-                break
+    def get_event_watch_name(self, event):
+        return event.name
+
+    def is_directory_event(self, event):
+        return event.mask & f.ISDIR
+
+    def is_event_self_removed(self, event):
+        return event.mask & self.flag_groups['self_delete']
+
+    def is_event_directory_added(self, event):
+        return self.is_directory_event(event) and (event.mask & self.flag_groups['added'])
+
+    def is_event_directory_removed(self, event):
+        return self.is_directory_event(event) and (event.mask & self.flag_groups['removed'])
+
+    def is_file_added(self, event):
+        return not self.is_directory_event(event) and (event.mask & self.flag_groups['added'])
+
+    def is_file_removed(self, event):
+        return not self.is_directory_event(event) and (event.mask & self.flag_groups['removed'])
+
+    def is_file_changed(self, event):
+        return not self.is_directory_event(event) and (event.mask & self.flag_groups['changed'])
 
 
 class FILTER_DENY: pass
@@ -2035,7 +2295,7 @@ class Key(Entity):
                 elif ref_conf:
                     KeyTextLine.add_waiting_reference(self, path, ref_conf)
 
-    def on_directory_removed(self, directory, flags):
+    def on_directory_removed(self, directory):
         pass
 
     @staticmethod
@@ -2286,7 +2546,7 @@ class Page(Entity):
                 elif ref_conf:
                     Key.add_waiting_reference(self, path, ref_conf)
 
-    def on_directory_removed(self, directory, flags):
+    def on_directory_removed(self, directory):
         pass
 
     @staticmethod
@@ -2437,7 +2697,7 @@ class Deck(Entity):
                         return None
                     return self.on_child_entity_change(path=path, flags=flags, entity_class=Page, data_identifier=main['page'], args=args, ref_conf=ref_conf, ref=ref, modified_at=modified_at)
 
-    def on_directory_removed(self, directory, flags):
+    def on_directory_removed(self, directory):
         self.unrender()
         self.end_reason = (1, f'[{self}] Deck configuration directory "{directory}" was removed')
         self.end_event.set()
@@ -2770,20 +3030,24 @@ class Manager:
     def add_watch(cls, directory, owner):
         if not cls.files_watcher:
             return
-        cls.files_watcher.add_watch(directory, owner)
+        WatchedDirectory.add(cls.files_watcher, directory, owner)
 
     @classmethod
     def remove_watch(cls, directory, owner):
         if not cls.files_watcher:
             return
-        cls.files_watcher.remove_watch(directory, owner)
+        WatchedDirectory.remove(directory, owner)
+
+    @classmethod
+    def get_files_watcher_class(cls):
+        return InotifyFilesWatcher
 
     @classmethod
     def start_files_watcher(cls):
         if cls.files_watcher:
             return
-        cls.files_watcher = Inotifier()
-        cls.files_watcher_thread = threading.Thread(name=cls.files_watcher.name, target=cls.files_watcher.run)
+        cls.files_watcher = cls.get_files_watcher_class()()
+        cls.files_watcher_thread = threading.Thread(name=cls.files_watcher.thread_name, target=cls.files_watcher.run)
         cls.files_watcher_thread.start()
 
     @classmethod
