e4a0a403e2bb39b979e490bb726b72fa3e027cae
Stephane Angel (Twidi) <s.angel@twidi.com>
2021-06-06 01:27:16 +0200

    Remove useless brightness command

--- streamdeckify/commands/__init__.py
+++ streamdeckify/commands/__init__.py
@@ -11,7 +11,6 @@ from .inspect import inspect  # noqa: F401
 from .make_dirs import make_dirs  # noqa: F401
 from .run import run  # noqa: F401
 from .api import *  # noqa: F401, F403
-from .brightness import brightness  # noqa: F401
 
 from .base import cli
 
